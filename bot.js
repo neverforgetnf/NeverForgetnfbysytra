6a2f2410a80a9980c8c4b6fbc0dba78c5e3c3e5d
neverforgetnf <41763030+neverforgetnf@users.noreply.github.com>
2018-08-05 11:19:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = '*'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -27,5 +27,81 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'السلام عليكم') {
+              message.channel.send('**ۅ عـڵـﯾ̃ڪمـ ٱڵـسڵـٱمـ ۅږحمـﮤ ٱڵـڵـھ ۅﭜږڪٱﭤھ**');
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'باك') {
+              message.channel.send('**وِلِـكُمِـ ﻧَوِرُتْ اٌلِـسُيٌـرُفّرُ يٌـاٌ حٍلِـوِ**');
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'برب') {
+              message.channel.send('**تْيٌـتْ , تْرُجِعٍ بّـاٌلِـسُلِـاٌمِـةُ**');
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === '.') {
+              message.channel.send('**اطﻟﻗ̮ـ̃ ﻧﻗ̮ـ̃طھَہّھَہّ**');
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'الكلمة') {
+              message.channel.send('**الرد**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+         client.on('message', message => {
+            if (message.content === 'الكلمة') {
+              message.channel.send('**الرد**');
+              message.channel.sendFile("./photoshop.PNG");
+               
+
+            }
+});
+
+
+
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
