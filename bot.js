d37c70be7cf26350bf0e865e902494fead22d8af
neverforgetnf <41763030+neverforgetnf@users.noreply.github.com>
2018-08-05 17:19:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -557,26 +557,28 @@ client.on('message', message => {
 
 	 
 
-var prefix = "*";
-
 client.on('message', message => {
+
+  var prefix = "*";    //prefix
+
   if (message.author.bot) return;
+
   if (!message.content.startsWith(prefix)) return;
 
   let command = message.content.split(" ")[0];
+
   command = command.slice(prefix.length);
 
   let args = message.content.split(" ").slice(1);
-  
- 
 
-if (command == "say") {
-    let say = new Discord.RichEmbed()
-    .setDescription(args.join("  "))
-    .setColor(0x23b2d6)
-    message.channel.sendEmbed(say);
-    message.delete();
+  if (command === "say") {
+
+          message.delete()
+
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+
   }
-});
+
+});  
 
 client.login(process.env.BOT_TOKEN);
